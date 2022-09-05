aef6c71c74f6cefe9a503fc768039e97cfa102aa
designneexx <webmasterondesign@gmail.com>
2022-09-06 00:02:04 +0300

    test-server исправляет баги

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const { JavaCaller } = require('java-caller')
 
 const app = express()
 
-const java = new JavaCaller({
-  jar: 'minecraft_server.1.16.1.jar',
-})
-
-java
-  .run(['-Xmx1024M', '-Xms1024M', 'nogui'])
-  .then((data) => {
-    console.log('success', data)
-  })
-  .catch((err) => {
-    console.error(err)
-  })
-
 app.listen(process.env.PORT || 3007, (data) => {
+  const java = new JavaCaller({
+    jar: 'mcserver.jar',
+  })
+
+  java
+    .run(['-Xmx1024M', '-Xms1024M', 'nogui'])
+    .then((data) => {
+      console.log('success', data)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+
   console.log(`Example app listening on port ${data}`)
 })
 
